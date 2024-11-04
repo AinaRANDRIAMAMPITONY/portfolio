ac95e1faf62c145f583065cf9617a034423fc575
Aina <ainarahajo12@gmail.com>
2024-11-04 12:26:00 +0300

    first commit portfolio

---
+++ js/cvweb.js
@@ -0,0 +1,17 @@
+/*show/hide skills item
+const skillItems = document.querySelectorAll("section.skills .skill");
+
+skillItems.forEach((skill) => {
+  skill.querySelector(".head").addEventListener("click", () => {
+    skill.querySelector(".items").classList.toggle(".show-items");
+    console.log("mandeha ny code");
+  });
+});
+
+
+$(document).ready(function () {
+  $(".head").click(function () {
+    $(".collapse").collapse("toggle");
+  });
+});
+*/
